0c9bf7fe984d6ab5f96e24b29659f8f04f93ca15
prasadi <prasadi181195@gmail.com>
2026-06-11 15:05:16 +0400

    Added mvn configurations

--- hybridAutomation/src/main/java/ui/base/BaseUI.java
+++ hybridAutomation/src/main/java/ui/base/BaseUI.java
@@ -8,6 +8,8 @@ import utils.ConfigReader;
 
 
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
 
 public class BaseUI {
 
@@ -16,19 +18,19 @@ public class BaseUI {
 
     public void initUI() {
         ChromeOptions options = new ChromeOptions();
-//        Map<String, Object> prefs = new HashMap<>();
-//        prefs.put("credentials_enable_service", false);
-//        prefs.put("profile.password_manager_enabled", false);
-//
-//        options.setExperimentalOption("prefs", prefs);
-//        options.addArguments("--incognito");
-//        options.addArguments("--disable-notifications");
-//        options.addArguments("--disable-save-password-bubble");
-//        options.addArguments("--headless=new");
-//        options.addArguments("--no-sandbox");
-//        options.addArguments("--disable-dev-shm-usage");
-//        driver = new ChromeDriver(options);
-        driver = new ChromeDriver();
+        Map<String, Object> prefs = new HashMap<>();
+        prefs.put("credentials_enable_service", false);
+        prefs.put("profile.password_manager_enabled", false);
+
+        options.setExperimentalOption("prefs", prefs);
+        options.addArguments("--incognito");
+        options.addArguments("--disable-notifications");
+        options.addArguments("--disable-save-password-bubble");
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        driver = new ChromeDriver(options);
+//        driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get(ConfigReader.get("appURL"));
         driver.manage().window().maximize();
